2f6fbaf3f032a75a5f114a32191d30ec40eb5cf4
bbmymy <www.afanas.oleg@gmail.com>
2025-04-04 08:22:27 +0300

    Update задача1.c

--- задача1.c
+++ задача1.c
@@ -1,1 +1,11 @@
-#include 
+#include <stdio.h>
+
+int main() {
+    char cvet[3][10] = {"красный", "синий", "зеленый"};
+
+    for(int i = 0; i < 3; i++) {
+        printf("%s\n", cvet[i]); // Исправлено на cvet[i]
+    }
+    
+    return 0;
+}
